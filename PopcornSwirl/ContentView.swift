bdb195df7227fbf92840d30bfceafbd8007968c8
Gabriel Balta <g.balta96@gmail.com>
2020-08-23 16:02:33 +0100

    Initial commit

---
+++ PopcornSwirl/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PopcornSwirl
+//
+//  Created by Gabriel Balta on 23/08/2020.
+//  Copyright © 2020 Gabriel Balta. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
