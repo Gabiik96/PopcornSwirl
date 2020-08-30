0d7d3d63b5594c9eeec2b8937ec0a6901b6913e3
Gabriel Balta <g.balta96@gmail.com>
2020-08-30 23:47:57 +0100

    Fonts added

---
+++ PopcornSwirl/Extensions/PlainButtonStyle.swift
@@ -0,0 +1,14 @@
+//
+//  PlainButtonStyle.swift
+//  PopcornSwirl
+//
+//  Created by Gabriel Balta on 30/08/2020.
+//  Copyright © 2020 Gabriel Balta. All rights reserved.
+//
+
+import SwiftUI
+
+extension PlainButtonStyle {
+    
+    
+}
