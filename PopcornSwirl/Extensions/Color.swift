f7fca9c81b0aa627af80e41968693d46c4045f70
Gabriel Balta <g.balta96@gmail.com>
2020-08-30 23:47:43 +0100

    Color added

---
+++ PopcornSwirl/Extensions/Color.swift
@@ -0,0 +1,57 @@
+//
+//  Color.swift
+//  PopcornSwirl
+//
+//  Created by Gabriel Balta on 29/08/2020.
+//  Copyright © 2020 Gabriel Balta. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    public static var steam_white: Color {
+        Color("steam_white", bundle: nil)
+    }
+    
+    public static var steam_gold: Color {
+        Color("steam_gold", bundle: nil)
+    }
+    
+    public static var steam_rust: Color {
+        Color("steam_rust", bundle: nil)
+    }
+    
+    public static var steam_rust2: Color {
+        Color("steam_rust2", bundle: nil)
+    }
+    
+    public static var steam_bronze: Color {
+        Color("steam_bronze", bundle: nil)
+    }
+    
+    public static var steam_brown: Color {
+        Color("steam_brown", bundle: nil)
+    }
+    
+    public static var steam_yellow: Color {
+        Color("steam_yellow", bundle: nil)
+    }
+    
+    public static var steam_blue: Color {
+        Color("steam_blue", bundle: nil)
+    }
+    
+    public static var steam_bordeaux: Color {
+        Color("steam_bordeaux", bundle: nil)
+    }
+    
+    public static var steam_green: Color {
+        Color("steam_green", bundle: nil)
+    }
+    
+    public static var steam_back: Color {
+        Color("steam_back", bundle: nil)
+    }
+}
+
