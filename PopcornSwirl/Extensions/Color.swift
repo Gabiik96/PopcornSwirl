9e05dfb54f008c58d876a0792903e64d9ea92fee
Gabriel Balta <g.balta96@gmail.com>
2020-10-03 19:09:12 +0100

    Colors updated

--- PopcornSwirl/Extensions/Color.swift
+++ PopcornSwirl/Extensions/Color.swift
@@ -9,48 +9,28 @@ import Foundation
 import SwiftUI
 
 extension Color {
-    public static var steam_white: Color {
-        Color("steam_white", bundle: nil)
+    public static var popcorn_white: Color {
+        Color("popcorn_white", bundle: nil)
     }
     
-    public static var steam_gold: Color {
-        Color("steam_gold", bundle: nil)
+    public static var popcorn_gold: Color {
+        Color("popcorn_gold", bundle: nil)
     }
     
-    public static var steam_rust: Color {
-        Color("steam_rust", bundle: nil)
+    public static var popcorn_gray: Color {
+        Color("popcorn_gray", bundle: nil)
     }
     
-    public static var steam_rust2: Color {
-        Color("steam_rust2", bundle: nil)
+    public static var popcorn_red: Color {
+        Color("popcorn_red", bundle: nil)
     }
     
-    public static var steam_bronze: Color {
-        Color("steam_bronze", bundle: nil)
+    public static var popcorn_green: Color {
+        Color("popcorn_green", bundle: nil)
     }
     
-    public static var steam_brown: Color {
-        Color("steam_brown", bundle: nil)
-    }
-    
-    public static var steam_yellow: Color {
-        Color("steam_yellow", bundle: nil)
-    }
-    
-    public static var steam_blue: Color {
-        Color("steam_blue", bundle: nil)
-    }
-    
-    public static var steam_red: Color {
-        Color("steam_red", bundle: nil)
-    }
-    
-    public static var steam_green: Color {
-        Color("steam_green", bundle: nil)
-    }
-    
-    public static var steam_back: Color {
-        Color("steam_back", bundle: nil)
+    public static var popcorn_black: Color {
+        Color("popcorn_back", bundle: nil)
     }
 }
 
