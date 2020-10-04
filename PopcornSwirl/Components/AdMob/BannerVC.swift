294949afb8c2f60330981de6678e46f0a0f86676
Gabriel Balta <g.balta96@gmail.com>
2020-10-04 01:12:35 +0100

    AdMob Init

---
+++ PopcornSwirl/Components/AdMob/BannerVC.swift
@@ -0,0 +1,20 @@
+//
+//  BannerVC.swift
+//  PopcornSwirl
+//
+//  Created by Gabriel Balta on 04/10/2020.
+//
+
+import SwiftUI
+
+struct BannerVC: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BannerVC_Previews: PreviewProvider {
+    static var previews: some View {
+        BannerVC()
+    }
+}
